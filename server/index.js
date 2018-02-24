3cb00737744c67ff5c3f5c22ea7e9832137a16b9
Ethan Fung <EthanEFung@gmail.com>
2018-02-24 09:13:02 -0800

    chore: define and serve static files

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ const port = process.env.PORT || 3000;
 app
   .use(urlencoded({ extended: true }))
   .use(json())
-  // .use(express.static(path.resolve('public')))
-  .use(router)
+  .use(express.static(path.resolve('public')))
+  .use(router);
 
 app.listen(port, err => {
   if (err) throw `could not connect to port ${port}`;
   console.log(`listening on port ${port}`);
-})  
+});  
