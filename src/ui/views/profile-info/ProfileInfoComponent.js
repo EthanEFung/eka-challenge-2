344b8181b60ff018b4abf9c4823022841bc3e88d
Ethan Fung <EthanEFung@gmail.com>
2018-02-24 11:04:56 -0800

    feat: create profile info component

---
+++ src/ui/views/profile-info/ProfileInfoComponent.js
@@ -0,0 +1,39 @@
+
+
+import React from 'react';
+import classNames from 'classnames';
+import {
+  Button,
+  FlexBox,
+  Header,
+  Input,
+} from '../../components';
+
+export const ProfileInfoComponent = props => {
+  const { children, className } = props;
+  const classes = classNames('credentials', 'vertical', className);
+
+  return (
+    <FlexBox className={classes} >
+      <Input
+        id="first-name"
+        className="first-name"
+        label="first name"
+      />
+      <Input
+        id="last-name"
+        className="last-name"
+        label="last name"
+      />
+      <Input
+        id="phone-number"
+        className="phone-number"
+        label="phone number"
+      />
+      <Button className="save" >Save</Button>
+    </FlexBox>
+  );
+};
+
+
+
