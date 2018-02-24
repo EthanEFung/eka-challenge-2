29425a4f0c6dea03756be628b151d0d26753afd6
Ethan Fung <EthanEFung@gmail.com>
2018-02-24 11:21:55 -0800

    chore: export views

--- src/ui/views/index.js
+++ src/ui/views/index.js
@@ -1,1 +1,5 @@
+export { CheckoutComponent } from './checkout'
+export { CredentialsComponent } from './credentials';
 export { HomeComponent } from './home';
+export { ProfileInfoComponent } from './profile-info';
+export { UserAddressInfoComponent } from './user-address-info';
