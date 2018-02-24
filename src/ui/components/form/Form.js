501ed67f3631eb3665c9199620fdc99c9210049d
Ethan Fung <EthanEFung@gmail.com>
2018-02-24 11:04:11 -0800

    refactor: form elements displayed vertically

--- src/ui/components/form/Form.js
+++ src/ui/components/form/Form.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import classNames from 'classnames';
+import { FlexBox } from '../';
 
 export const Form = props => {
   const { children, className } = props;
   const classes = classNames('form', className);
 
-
-
   return (
     <form className={classes}>
-      {children}
+      <FlexBox className="vertical" >
+        {children}
+      </FlexBox>
     </form>
   );
 };
