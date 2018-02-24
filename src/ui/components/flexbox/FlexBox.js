0fd143c87743107ea8e18664450db53e3808491c
Ethan Fung <EthanEFung@gmail.com>
2018-02-24 10:06:51 -0800

    refactor: allow flexbox component to accept classes

--- src/ui/components/flexbox/FlexBox.js
+++ src/ui/components/flexbox/FlexBox.js
@@ -1,12 +1,14 @@
 
 import React from 'react';
+import classNames from 'classnames';
+
 import './FlexBox.css';
 
 export const FlexBox = props => {
   const { children, className } = props;
-
+  const classes = classNames("flex-box", className)
   return (
-    <div className="flex-box">
+    <div className={classes}>
       {children}
     </div>
   );
