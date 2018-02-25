46701b2cd88ffa1fd34c86733b9a22e95a9ca0b8
Ethan Fung <EthanEFung@gmail.com>
2018-02-25 08:47:10 -0800

    clean: clean src index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-// this is the glue that holds it all together
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, } from 'redux';
 import { Provider } from 'react-redux'
-
 
 import { AppComponent, AppContainer } from './ui'
 
