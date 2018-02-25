b33cfc732424e5cbc092a8c8c525403d0f9d8939
Ethan Fung <EthanEFung@gmail.com>
2018-02-25 08:29:44 -0800

    chore: profide footer export from components folder

--- src/ui/components/index.js
+++ src/ui/components/index.js
@@ -2,5 +2,6 @@
 export { Button } from './button';
 export { FlexBox } from './flexbox';
 export { Form } from './form';
+export { Footer } from './footer';
 export { Header } from './header';
 export { Input } from './input';
