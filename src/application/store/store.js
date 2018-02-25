fbca09c58bf7d30b519714d1f2d82cd893064ab7
Ethan Fung <EthanEFung@gmail.com>
2018-02-25 08:26:10 -0800

    chore: configure app reducer

--- src/application/store/store.js
+++ src/application/store/store.js
@@ -3,11 +3,13 @@ import { applyMiddleware, combineReducers, createStore, } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
+import appReducer from './appReducer';
 import credentialsReducer from './credentialsReducer';
 import profileInfoReducer from './profileInfoReducer';
 import userAddressInfoReducer from './userAddressInfoReducer';
 
 const reducers = combineReducers({
+  app: appReducer,
   credentials: credentialsReducer,
   profileInfo: profileInfoReducer,
   userAddressInfo: userAddressInfoReducer,
